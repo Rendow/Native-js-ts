44c9a8969bd6258e5a738772a1be03bf4d78ee18
Rendow <rendow33@gmail.com>
2021-03-05 19:53:01 +0300

    commit

--- src/10/10-01.tsx
+++ src/10/10-01.tsx
@@ -11,6 +11,14 @@ export type LaptopType = {
 export type UserWithLaptop = UserType & {
     laptop: LaptopType
 }
+export type UserWithBooks = UserType & {
+    laptop: LaptopType
+    books:Array<string>
+}
+export type UserWithCompanies = UserType & {
+    laptop: LaptopType
+    companies:Array<{ id:number, title:string }>
+}
 //аналогичная запись
 /*export type UserWithLaptop = {
     laptop: LaptopType
@@ -39,3 +47,69 @@ export function moveUser(u:UserType, city:string){
     }
     return copy
 }
+
+export const updateCompanyTitle = (user:UserWithCompanies,
+                                   companyID:number,
+                                   newTitle:string) => {
+    return  {
+        ...user,
+        companies:user.companies.map
+        (c => c.id === companyID ? {...c,title:newTitle} : c)
+    }
+}
+//analog
+export const updateCompanyTitleTwo = (user:UserWithCompanies,
+                                    companyID:number,
+                                    newTitle:string) => {
+    const copy:UserWithCompanies = {
+        ...user,
+        companies:user.companies.map(c => {
+            if (c.id === companyID){
+                return {...c,title:newTitle}
+            } else return c
+        })
+    }
+    return copy
+}
+
+export const addNewBooks = (user:UserWithBooks, newBook:string) => {
+  return {
+       ...user,
+   books:[...user.books, newBook]
+   }
+}
+/*
+    books:[...user.books, newBook]
+ , newBook - аналог пуш (  copy.books.push(newBook)   *)
+ /
+ */
+
+
+export const updateBook = (user:UserWithBooks,oldBook:string, newBook:string) => {
+    return {
+        ...user,
+        books:user.books.map(b=>{
+            if( b === oldBook){
+                return newBook
+            }
+            return b
+        })
+    }
+}
+//analog
+export const updateBookTwo = (user:UserWithBooks,oldBook:string, newBook:string) => {
+    return {
+        ...user,
+   books:user.books.map(b=> b === oldBook ? newBook : b)
+    }
+}
+
+export const deleteBook = (user:UserWithBooks, newBook:string) => {
+    return {
+        ...user,
+        books:user.books.filter(b=> b !== newBook)
+    }
+}
+
+
+
--- src/10/10.test.ts
+++ src/10/10.test.ts
@@ -1,4 +1,14 @@
-import {makeHairStyle, moveUser, UserType, UserWithLaptop} from "./10-01";
+import {
+    addNewBooks,
+    deleteBook,
+    makeHairStyle,
+    moveUser, updateBook,
+    updateCompanyTitle,
+    UserType,
+    UserWithBooks,
+    UserWithCompanies,
+    UserWithLaptop
+} from "./10-01";
 
 
 test('test hair',() => {
@@ -37,5 +47,102 @@ test('change address',() => {
 
     expect(user.laptop).toBe(movedUser.laptop)
     expect(movedUser.address.city).toBe('kiev')
+
+})
+
+test('change company',() => {
+    let user:UserWithCompanies = {
+        name: 'name',
+        hair:32,
+        address:{
+            city:'minsk',
+            house:12
+        },
+        laptop:{
+            title:'asus'
+        },
+        companies:[{id:1, title:'Епам'},{id:2, title: 'it-incubator'}]
+    }
+
+    const userCopy = updateCompanyTitle(user, 1, 'EPAM') as UserWithCompanies
+
+
+    expect(user).not.toBe(userCopy)
+    expect(user.address).toBe(userCopy.address)
+
+    expect(user.companies).not.toBe(userCopy.companies)
+    expect(userCopy.companies[0].title).toBe('EPAM')
+
+})
+test('add new book',() => {
+    let user:UserWithBooks = {
+        name: 'name',
+        hair:32,
+        address:{
+            city:'minsk',
+            house:12
+        },
+        laptop:{
+            title:'asus'
+        },
+        books: ['css','js','react']
+    }
+
+    const userCopy = addNewBooks(user, 'ts')
+
+    expect(user).not.toBe(userCopy)
+    expect(user.address).toBe(userCopy.address)
+
+    expect(user.books).not.toBe(userCopy.books)
+    expect(user.books.length).toBe(3)
+
+})
+
+test('update book',() => {
+    let user:UserWithBooks = {
+        name: 'name',
+        hair:32,
+        address:{
+            city:'minsk',
+            house:12
+        },
+        laptop:{
+            title:'asus'
+        },
+        books: ['css','js','react']
+    }
+
+    const userCopy = updateBook(user,'js', 'ts')
+
+    expect(user).not.toBe(userCopy)
+    expect(user.address).toBe(userCopy.address)
+    expect(userCopy.books[1]).toBe('ts')
+
+    expect(user.books).not.toBe(userCopy.books)
+    expect(user.books.length).toBe(3)
+
+})
+
+test('delete book',() => {
+    let user:UserWithBooks = {
+        name: 'name',
+        hair:32,
+        address:{
+            city:'minsk',
+            house:12
+        },
+        laptop:{
+            title:'asus'
+        },
+        books: ['css','js','react']
+    }
+
+    const userCopy = deleteBook(user,'js')
+
+    expect(user).not.toBe(userCopy)
+    expect(user.address).toBe(userCopy.address)
+
+    expect(user.books).not.toBe(userCopy.books)
+    expect(userCopy.books.length).toBe(2)
 
 })
